c1d5cdcc0fa7d757e8cea5c732b5cb8d844008d3
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-09-15 17:23:05 -0500

    Initial Commit

---
+++ ImageClassificationApp/ImageClassificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationAppApp.swift
+//  ImageClassificationApp
+//
+//  Created by student on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
